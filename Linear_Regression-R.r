9e4e2c2dce75e5980183b6c76aab49d429c82fd4
Motilal-Uttarkabat <muttarkabat@gmail.com>
2021-07-05 18:57:44 +0000

    R-script for linear regression from input csv

---
+++ Linear_Regression-R.r
@@ -0,0 +1,18 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+library(plotly)                                        # Loading library for linear regression and interactive plots 
+library(ggplot2)                                       # Loading library for quick and easy plots
+
+data <- read.csv(args)                                 # Reading the CSV file from iput argument from command line
+
+png("r_orig.png")                                      # Output to a png file
+fig1 <- ggplot(data, aes(x = x, y = y)) + geom_point() # Scatter plot from data, aes-axes, geom_point-marker
+print(fig1)                                            # Printing the plot to the output png file
+dev.off()
+
+# Adding linear fit
+png("r_lm.png")                                        # Output to a png file
+fig2 <- fig1 + geom_point(color = "red") + geom_smooth(method = "lm", formula ="y ~ x", se = FALSE)               
+print(fig2)                                            # Printing the plot to the output png file
+dev.off()
