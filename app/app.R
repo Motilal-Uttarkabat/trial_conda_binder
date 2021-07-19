4de51f48b772b83f6f915976bed756d20c0db584
Motilal-Uttarkabat <86322287+Motilal-Uttarkabat@users.noreply.github.com>
2021-07-18 22:00:16 -0400

    Minor

--- app/app.R
+++ app/app.R
@@ -1,25 +1,23 @@
-# Create Shiny app ----
-# Importing required library for plotting and shiny
-library(shiny)
-library(ggplot2)
-library(ggpmisc)
-# Defining UI for the application
-# The app uses code (slightly modified) for reading a csv to shiny app from Source- https://shiny.rstudio.com/gallery/file-upload.html
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
 
+library(shiny)
+library(plotly)
+# Define UI for application that draws a histogram
 ui <- fluidPage(
-    titlePanel("Polynomial Regression"),      # Title of the app output
     
-    # Sidebar layout with input and output definitions ----
+    # Application title
+    titlePanel("Linear Regression in R"),
+    
+    # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        
-        # Sidebar panel for inputs ----
         sidebarPanel(
             
-            sliderInput("Poly",
-                        "Order of polynomial model:",
-                        min = 0,
-                        max = 10,
-                        value = 0),
             
             # Input: Select a file ----
             fileInput("file1", "Choose CSV File",
@@ -33,7 +31,7 @@ ui <- fluidPage(
             
             # Input: Checkbox if file has header ----
             checkboxInput("header", "Header", TRUE),
-            checkboxInput("se", "Confidence Interval", TRUE),
+            
             # Input: Select separator ----
             radioButtons("sep", "Separator",
                          choices = c(Comma = ",",
@@ -41,37 +39,42 @@ ui <- fluidPage(
                                      Tab = "\t"),
                          selected = ","),
             
+            # Input: Select quotes ----
+            radioButtons("quote", "Quote",
+                         choices = c(None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"),
+                         selected = '"'),
+            
             # Horizontal line ----
             tags$hr(),
             
             # Input: Select number of rows to display ----
-            radioButtons("disp", "Display",
+            radioButtons("disp", "Display Data",
                          choices = c(Head = "head",
                                      All = "all"),
                          selected = "head"),
-            tags$hr(),
-            textOutput("Export"),
-            numericInput("width", "Width (inches)", value = 7, min = 1, max = 20),
-            numericInput("height", "Height (inches)", value = 7, min = 1, max = 20),
-            numericInput("dpi", "DPI", value = 150, min = 72, max = 300),
-            downloadButton('Download')
+            actionButton("lm", "Linear Fit"),
+            htmlOutput("RSquared"),
+            htmlOutput("Slope"),
+            htmlOutput("Intercept")
         ),
         
-        # Main panel for displaying outputs ----
+        # Show a plot of the generated distribution
         mainPanel(
-            # Output scatter Plot with regression
-            plotOutput("scatterPlot"),
+            plotlyOutput("ScatterPlot"),
+            tags$hr(),
+            tags$hr(),
+            plotlyOutput("fitPlot"),
+            plotlyOutput("residualPlot"),
             tableOutput("contents")
         )
     )
 )
 
-# Define server logic to read selected file
-# Logic for output of a scatter plot with linear regression from a csv input
-
+# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    # Reactive dataframe variable from csv input
+    
     dataInput <- reactive({
         req(input$file1)
         df <- read.csv(input$file1$datapath,
@@ -81,38 +84,46 @@ server <- function(input, output) {
         return(df)
     })
     
-    # Parsing data from csv to a Reactive dat() for plotting
-    dat <- reactive({
-        req(input$file1)
-        df <- read.csv(input$file1$datapath,header = input$header,sep = input$sep)
-        df
+    Line <- eventReactive(input$lm,{lm(dataInput()$y ~ dataInput()$x)})
+
+    output$ScatterPlot <- renderPlotly({
+        plot <- plot_ly(dataInput(), x = ~x, y = ~y, type = 'scatter', mode = 'markers')%>%
+            layout(title="Scatter Plot")
     })
     
-    # Scatter plot with regressional analysis using stat_smooth (ggplot2) and stat_poly_eq (from ggpmisc)
-    output$scatterPlot <- renderPlot({figure()})
-        
-    figure <- reactive ({
-        ggplot(dat(),aes(x=x,y=y))+geom_point(colour='red')+stat_smooth(                                    # Scatter Plot 
-            method = "lm",                                                                                  # Fitting/smoothing to a polynomial fit
-            formula = y ~ poly(x, input$Poly, raw = T),                                                     # Formula for the polynomial fit from input
-            size = 1,                                                                                       # Line width of the fit
-            se=input$se)+stat_poly_eq(                                                                             # Function of equation and r squared value
-                formula = y ~ poly(x, input$Poly, raw = T),
-                aes(label=paste(..eq.label.., ..rr.label.., sep = "~~~")),
-                parse = T, label.y="top", label.x="left")
+    output$fitPlot <- renderPlotly({
+        plot <- plot_ly(dataInput(), x = ~x, y = ~y, type = 'scatter', mode = 'markers') %>% 
+            add_trace(dataInput(), x = ~x, y = fitted(Line()), mode = "lines", showlegend = F) %>%
+                layout(title="Linear Fit")
     })
     
-    output$Download <- downloadHandler(
-    filename = function() { paste(tools::file_path_sans_ext(input$file1), 'order', input$Poly, '.png', sep='') },
-    content = function(file) {
-        ggsave(file, plot = figure(), width = input$width, height = input$height, dpi = input$dpi, units = "in", device = "png")
+    output$residualPlot <- renderPlotly({
+        plot <- plot_ly(dataInput(), x = ~x, y = resid(Line()), type = 'scatter', mode = 'markers') %>% 
+            layout(title="Residuals (linear model)")
     })
     
-    output$Export <- renderText(
-        paste("Download")
-    )
+    output$RSquared <- renderUI({
+        str1 <- paste("R", tags$sup(2),"-", sep = "")
+        str2 <- paste(format(round(summary(Line())$r.squared, 3)))
+        HTML(paste(str1, str2))
+    })
+    output$Slope <- renderUI({
+        str1 <- paste("Slope-")
+        str2 <- paste(format(round(summary(Line())$coefficients[2], 3)))
+        HTML(paste(str1, str2))
+    })
+    
+    output$Intercept <- renderUI({
+        str1 <- paste("Intercept-")
+        str2 <- paste(format(round(summary(Line())$coefficients[1], 3)))
+        HTML(paste(str1, str2))
+    })
     
     output$contents <- renderTable({
+        
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
         if(input$disp == "head") {
             return(head(dataInput()))
         }
@@ -121,5 +132,6 @@ server <- function(input, output) {
         }
     })
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
